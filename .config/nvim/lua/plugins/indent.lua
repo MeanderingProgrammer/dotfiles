5aafa918e632ef9be8bcc2dd4483075b59c1e856
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-09-10 20:03:01 -0700

    Use dashboard plugin rather than default, update indent to ignore dashboards

--- .config/nvim/lua/plugins/indent.lua
+++ .config/nvim/lua/plugins/indent.lua
@@ -2,7 +2,9 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
         require('indent_blankline').setup({
+            use_treesitter = true,
             show_trailing_blankline_indent = false,
+            filetype_exclude = { 'dashboard' },
         })
     end,
 }
