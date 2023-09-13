9efee681cc4f40a74699718c2552df0c3fd9538c
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-09-12 19:25:16 -0700

    Run setup of custom dashboard

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,15 +1,10 @@
 return {
-    --'glepnir/dashboard-nvim',
-    --event = 'VimEnter',
-    --dependencies = {
-    --    'nvim-tree/nvim-web-devicons',
-    --},
-    --config = function()
-    --    require('dashboard').setup({
-    --        config = {
-    --            week_header = { enable = true },
-    --        },
-    --    })
-    --end,
     dir = '~/dev/repos/dashboard.nvim',
+    event = 'VimEnter',
+    dependencies = {
+        'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+        require('dashboard').setup()
+    end,
 }
