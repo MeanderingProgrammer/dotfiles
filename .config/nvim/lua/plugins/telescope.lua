6c22cd33a31c5a6ee3b6058654eae151149f928b
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-09-19 16:58:41 -0700

    Use fzf extension with telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,12 +3,15 @@ return {
     dependencies = {
         'nvim-lua/plenary.nvim',
         'debugloop/telescope-undo.nvim',
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     keys = {
         { '<leader>f', '<cmd>Telescope find_files<cr>', desc = 'Telescope: Find files' },
         { '<leader>g', '<cmd>Telescope live_grep<cr>', desc = 'Telescope: Grep files' },
     },
     config = function()
-        require('telescope').setup({})
+        local telescope = require('telescope')
+        telescope.setup({})
+        telescope.load_extension('fzf')
     end,
 }
