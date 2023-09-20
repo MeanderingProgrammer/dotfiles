3df42958b6a6199af3e7ee05f8adb33f9dd49624
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-09-19 19:17:44 -0700

    Provide dashboard with header

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -3,9 +3,11 @@ return {
     event = 'VimEnter',
     dependencies = {
         'nvim-tree/nvim-web-devicons',
+        { 'MaximilianLloyd/ascii.nvim', dependencies = { 'MunifTanjim/nui.nvim' } }
     },
     config = function()
         require('dashboard').setup({
+            header = require('ascii').art.text.neovim.sharp,
             directories = {
                 '~/.config',
                 '~/Documents/notes',
