37ff1cb5316c5100880679626a26db008516b266
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-09-25 22:18:50 -0700

    M Set up all mappings for harpoon core, keep around harpoon ones for comparison

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -9,15 +9,8 @@ return {
         local mark = require('harpoon.mark')
         local ui = require('harpoon.ui')
 
-        vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Harpoon: Add current file' })
-        vim.keymap.set('n', '<leader>r', mark.rm_file, { desc = 'Harpoon: Remove current file' })
-        vim.keymap.set('n', '<leader><leader>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle UI' })
-
-        for i = 1, 5 do
-            local open_file = function()
-                ui.nav_file(i)
-            end
-            vim.keymap.set('n', '<leader>' .. i, open_file, { desc = 'Harpoon: Open file ' .. i })
-        end
+        vim.keymap.set('n', '<C-a>', mark.add_file, { desc = 'Harpoon: Add current file' })
+        vim.keymap.set('n', '<C-r>', mark.rm_file, { desc = 'Harpoon: Remove current file' })
+        vim.keymap.set('n', '<C-d>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle UI' })
     end,
 }
--- .config/nvim/lua/plugins/harpooncore.lua
+++ .config/nvim/lua/plugins/harpooncore.lua
@@ -7,18 +7,18 @@ return {
     config = function()
         require('harpoon-core').setup({})
 
-        -- local mark = require('harpoon.mark')
-        -- local ui = require('harpoon.ui')
-        --
-        -- vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Harpoon: Add current file' })
-        -- vim.keymap.set('n', '<leader>r', mark.rm_file, { desc = 'Harpoon: Remove current file' })
-        -- vim.keymap.set('n', '<leader><leader>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle UI' })
-        --
-        -- for i = 1, 5 do
-        --     local open_file = function()
-        --         ui.nav_file(i)
-        --     end
-        --     vim.keymap.set('n', '<leader>' .. i, open_file, { desc = 'Harpoon: Open file ' .. i })
-        -- end
+        local mark = require('harpoon-core.mark')
+        local ui = require('harpoon-core.ui')
+
+        vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Harpoon: Add current file' })
+        vim.keymap.set('n', '<leader>r', mark.rm_file, { desc = 'Harpoon: Remove current file' })
+        vim.keymap.set('n', '<leader><leader>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle UI' })
+
+        for i = 1, 5 do
+            local open_file = function()
+                ui.nav_file(i)
+            end
+            vim.keymap.set('n', '<leader>' .. i, open_file, { desc = 'Harpoon: Open file ' .. i })
+        end
     end,
 }
