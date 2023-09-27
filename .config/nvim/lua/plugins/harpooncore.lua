717a3bf5f894de7c7e1b1f77c58a82951d0de079
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-09-27 00:47:31 -0700

    Add next & previous harpoon keymaps

--- .config/nvim/lua/plugins/harpooncore.lua
+++ .config/nvim/lua/plugins/harpooncore.lua
@@ -8,12 +8,13 @@ return {
         require('harpoon-core').setup({})
 
         local mark = require('harpoon-core.mark')
-        local ui = require('harpoon-core.ui')
-
         vim.keymap.set('n', '<leader>a', mark.add_file, { desc = 'Harpoon: Add current file' })
         vim.keymap.set('n', '<leader>r', mark.rm_file, { desc = 'Harpoon: Remove current file' })
-        vim.keymap.set('n', '<leader><leader>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle UI' })
 
+        local ui = require('harpoon-core.ui')
+        vim.keymap.set('n', '<leader><leader>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle UI' })
+        vim.keymap.set('n', '<leader>n', ui.nav_next, { desc = 'Harpoon: Next file' })
+        vim.keymap.set('n', '<leader>p', ui.nav_prev, { desc = 'Harpoon: Previous file' })
         for i = 1, 5 do
             local open_file = function()
                 ui.nav_file(i)
