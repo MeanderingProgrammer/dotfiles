c12399f5ebd34a9177614543a7023e34c52c7d73
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-04 04:09:59 -0700

    Disable missing-fields diagnostic in fterm

--- .config/nvim/lua/plugins/fterm.lua
+++ .config/nvim/lua/plugins/fterm.lua
@@ -5,6 +5,7 @@ return {
         { '<A-i>', '<esc><cmd>lua require("FTerm").toggle()<cr>', mode = 't', desc = 'FTerm: Toggle' },
     },
     config = function()
+        ---@diagnostic disable-next-line: missing-fields
         require('FTerm').setup({})
     end,
 }
