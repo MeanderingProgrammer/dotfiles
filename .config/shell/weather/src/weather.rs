3a96f5af70e4ea86cf0735465d96ce743f6ebe7b
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-04 06:42:11 -0700

    The other half of the previous commit

---
+++ .config/shell/weather/src/weather.rs
@@ -0,0 +1,67 @@
+use anyhow::Result;
+use chrono::{DateTime, Local};
+use reqwest::Client;
+use serde::{de::DeserializeOwned, Deserialize};
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+struct EndpointsProperties {
+    forecast_hourly: String,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct Endpoints {
+    properties: EndpointsProperties,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct ForecastPrecipitation {
+    pub value: u32,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct ForecastPeriod {
+    pub start_time: DateTime<Local>,
+    pub end_time: DateTime<Local>,
+    pub temperature: u32,
+    pub probability_of_precipitation: ForecastPrecipitation,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct ForecastProperties {
+    periods: Vec<ForecastPeriod>,
+}
+
+#[derive(Deserialize, Debug, Clone)]
+struct Forecast {
+    properties: ForecastProperties,
+}
+
+pub struct WeatherClient {
+    client: Client,
+}
+
+impl WeatherClient {
+    pub fn new(user_agent: &str) -> Result<Self> {
+        let client = Client::builder().user_agent(user_agent).build()?;
+        let weather_client = Self { client };
+        Ok(weather_client)
+    }
+
+    pub async fn get_endpoint(&self, long: &str, lat: &str) -> Result<String> {
+        let endpoint = format!("https://api.weather.gov/points/{},{}", long, lat);
+        let endpoints: Endpoints = self.get(&endpoint).await?;
+        Ok(endpoints.properties.forecast_hourly)
+    }
+
+    pub async fn get_forecast(&self, endpoint: &str) -> Result<Vec<ForecastPeriod>> {
+        let forecast: Forecast = self.get(endpoint).await?;
+        Ok(forecast.properties.periods)
+    }
+
+    async fn get<T: DeserializeOwned>(&self, endpoint: &str) -> Result<T> {
+        let result = self.client.get(endpoint).send().await?.json::<T>().await?;
+        Ok(result)
+    }
+}
