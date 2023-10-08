9f17b2aa73822dd37ca46b74aa8d01d3388650cb
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-08 14:27:47 -0700

    Minor edit to harpoon remaps

--- .config/nvim/lua/plugins/harpooncore.lua
+++ .config/nvim/lua/plugins/harpooncore.lua
@@ -15,7 +15,7 @@ return {
         vim.keymap.set('n', '<leader>r', mark.rm_file, { desc = 'Harpoon: Remove current file' })
 
         local ui = require('harpoon-core.ui')
-        vim.keymap.set('n', '<leader><leader>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle UI' })
+        vim.keymap.set('n', '<leader><leader><leader>', ui.toggle_quick_menu, { desc = 'Harpoon: Toggle UI' })
         vim.keymap.set('n', '<leader><leader>n', ui.nav_next, { desc = 'Harpoon: Next file' })
         vim.keymap.set('n', '<leader><leader>p', ui.nav_prev, { desc = 'Harpoon: Previous file' })
         for i = 1, 5 do
