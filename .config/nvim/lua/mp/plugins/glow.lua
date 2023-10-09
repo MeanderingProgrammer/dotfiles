43507afb393d271ec5f6c28de8106edbda212d54
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-09 06:03:07 -0700

    Remove glow, weird formatting

--- .config/nvim/lua/mp/plugins/glow.lua
+++
@@ -1,9 +0,0 @@
-return {
-    'ellisonleao/glow.nvim',
-    config = function()
-        ---@diagnostic disable-next-line: missing-fields
-        require('glow').setup({
-            width = 120,
-        })
-    end,
-}
