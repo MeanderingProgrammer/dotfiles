2caadabedc46bad0656394b10787d831984fbd73
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-27 23:37:39 -0700

    Move lualine into basic config

--- .config/nvim/lua/mp/plugins/basic.lua
+++ .config/nvim/lua/mp/plugins/basic.lua
@@ -1,6 +1,9 @@
+local icons = 'nvim-tree/nvim-web-devicons'
+
 return {
     { 'mg979/vim-visual-multi' },
     { 'numToStr/Comment.nvim', config = true },
+    { 'nvim-lualine/lualine.nvim', dependencies = { icons }, config = true },
     { 'tpope/vim-fugitive' },
     { 'tpope/vim-sleuth' },
 }
