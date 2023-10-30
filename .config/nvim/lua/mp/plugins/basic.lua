d78c36159c1a4430e9e1e9142396f4d354955820
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-10-29 23:37:27 -0700

    Move fterm plugin to basic

--- .config/nvim/lua/mp/plugins/basic.lua
+++ .config/nvim/lua/mp/plugins/basic.lua
@@ -1,8 +1,18 @@
 local icons = 'nvim-tree/nvim-web-devicons'
 
+local function load_fterm(mode, prefix)
+    local command = '<cmd>lua require("FTerm").toggle()<cr>'
+    return { '<A-i>', prefix .. command, mode = mode, desc = 'FTerm: Toggle' }
+end
+
 return {
     { 'mg979/vim-visual-multi' },
     { 'numToStr/Comment.nvim', config = true },
+    {
+        'numToStr/FTerm.nvim',
+        keys = { load_fterm('n', ''), load_fterm('t', '<esc>') },
+        config = true,
+    },
     { 'nvim-lualine/lualine.nvim', dependencies = { icons }, config = true },
     { 'tpope/vim-fugitive' },
     { 'tpope/vim-sleuth' },
