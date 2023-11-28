ac2075cea2c97d38c048715e353cee4f440437a6
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2023-11-27 19:51:34 -0800

    Disable italics in rose-pine colorscheme

--- .config/nvim/lua/mp/plugins/colorscheme.lua
+++ .config/nvim/lua/mp/plugins/colorscheme.lua
@@ -2,6 +2,9 @@ return {
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
+        require('rose-pine').setup({
+            disable_italics = true,
+        })
         vim.cmd('colorscheme rose-pine')
     end,
 }
