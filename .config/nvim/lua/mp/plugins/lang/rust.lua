0c9bb105e34d3d4fa937f5361b0d7ff69e43cfd5
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-02-15 15:21:19 -0800

    Update how dependency managers are configured for python & rust

--- .config/nvim/lua/mp/plugins/lang/rust.lua
+++ .config/nvim/lua/mp/plugins/lang/rust.lua
@@ -20,29 +20,28 @@ return {
         },
     },
     {
+        'Saecki/crates.nvim',
+        config = function()
+            ---@param lhs string
+            ---@param rhs fun()
+            ---@param desc string
+            local function map(lhs, rhs, desc)
+                vim.keymap.set('n', '<leader>' .. lhs, rhs, { silent = true, desc = 'Crates: ' .. desc })
+            end
+            local crates = require('crates')
+            map('ct', crates.toggle, 'Toggle UI')
+            map('cv', crates.show_versions_popup, 'Show Version Popup')
+            map('cd', crates.show_dependencies_popup, 'Show Dependency Popup')
+            map('cu', crates.upgrade_crate, 'Upgrade')
+            map('cU', crates.upgrade_all_crates, 'Upgrade All')
+            crates.setup({
+                src = { cmp = { enabled = true } },
+            })
+        end,
+    },
+    {
         'hrsh7th/nvim-cmp',
-        dependencies = {
-            {
-                'Saecki/crates.nvim',
-                config = function()
-                    ---@param lhs string
-                    ---@param rhs fun()
-                    ---@param desc string
-                    local function map(lhs, rhs, desc)
-                        vim.keymap.set('n', '<leader>' .. lhs, rhs, { silent = true, desc = 'Crates: ' .. desc })
-                    end
-                    local crates = require('crates')
-                    map('ct', crates.toggle, 'Toggle UI')
-                    map('cv', crates.show_versions_popup, 'Show Version Popup')
-                    map('cd', crates.show_dependencies_popup, 'Show Dependency Popup')
-                    map('cu', crates.upgrade_crate, 'Upgrade')
-                    map('cU', crates.upgrade_all_crates, 'Upgrade All')
-                    crates.setup({
-                        src = { cmp = { enabled = true } },
-                    })
-                end,
-            },
-        },
+        dependencies = { 'Saecki/crates.nvim' },
         opts = function(_, opts)
             table.insert(opts.sources, { name = 'crates' })
         end,
