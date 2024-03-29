9183298264de34a8c90f7fd97c930e7022e95220
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-03-29 15:12:36 -0700

    Do not change window size on font size changes

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,5 +14,6 @@ end)
 
 config.window_background_opacity = 0.9
 config.window_padding = { left = 0, right = 0, top = 0, bottom = 0 }
+config.adjust_window_size_when_changing_font_size = false
 
 return config
