702a90be48911cb6708693705ab1b59c9a77a85d
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-03-29 13:09:45 -0700

    Update trouble configuration

--- .config/nvim/lua/mp/plugins/trouble.lua
+++ .config/nvim/lua/mp/plugins/trouble.lua
@@ -3,16 +3,21 @@ return {
     branch = 'dev',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+        local mode_config = {
+            focus = false,
+            win = { position = 'bottom', size = 15 },
+        }
         require('trouble').setup({
-            -- Add width parameter to debug
-            width = 200,
+            modes = {
+                lsp = mode_config,
+                symbols = mode_config,
+            },
         })
         require('which-key').register({
             ['<leader>x'] = {
                 name = 'trouble',
-                l = { '<cmd>Trouble loclist toggle<cr>', 'Location List' },
-                q = { '<cmd>Trouble qflist toggle<cr>', 'Quickfix List' },
-                s = { '<cmd>Trouble symbols toggle focus=false<cr>', 'Symbols' },
+                l = { '<cmd>Trouble lsp toggle<cr>', 'LSP' },
+                s = { '<cmd>Trouble symbols toggle<cr>', 'Symbols' },
                 x = { '<cmd>Trouble diagnostics toggle<cr>', 'Diagnostics' },
                 X = { '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', 'Buffer Diagnostics' },
             },
