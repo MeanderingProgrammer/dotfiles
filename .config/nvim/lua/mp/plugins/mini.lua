6b297a124b90cbdec9883ab9c9f222ed21613bf8
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-03-30 19:47:53 -0700

    Add mini jump2d plugin

---
+++ .config/nvim/lua/mp/plugins/mini.lua
@@ -0,0 +1,12 @@
+return {
+    'echasnovski/mini.nvim',
+    config = function()
+        local jump2d = require('mini.jump2d')
+        jump2d.setup({
+            spotter = jump2d.builtin_opts.word_start.spotter,
+            allowed_windows = {
+                not_current = false,
+            },
+        })
+    end,
+}
