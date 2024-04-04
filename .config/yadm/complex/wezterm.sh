cd85e0cbedcba9c1d8c6a9f675dc3d735cef4248
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-04-04 13:28:01 -0700

    Add install steps for Wezterm on Ubuntu

---
+++ .config/yadm/complex/wezterm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://wezfurlong.org/wezterm/install/linux.html
+curl -fsSL https://apt.fury.io/wez/gpg.key | sudo gpg --yes --dearmor -o /usr/share/keyrings/wezterm-fury.gpg
+echo 'deb [signed-by=/usr/share/keyrings/wezterm-fury.gpg] https://apt.fury.io/wez/ * *' | sudo tee /etc/apt/sources.list.d/wezterm.list
+
+sudo apt update && sudo apt install wezterm
