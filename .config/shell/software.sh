4a7e2ef7b53917ad88824332c892374c5b587483
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-04-11 15:03:15 -0700

    Update vim command to behave differently depending on target being a file or a directory

--- .config/shell/software.sh
+++ .config/shell/software.sh
@@ -100,7 +100,13 @@ vim() {
     if [[ "${#}" -eq 0 ]]; then
         nvim
     elif [[ "${#}" -eq 1 ]]; then
-        cd "${1}" && nvim .
+        if [[ -d $1 ]]; then
+            cd "${1}" && nvim .
+        elif [[ -f $1 ]]; then
+            nvim "${1}"
+        else
+            echo "${1} is neither a file nor a directory"
+        fi
     else
         echo "Usage: <path>?"
     fi
