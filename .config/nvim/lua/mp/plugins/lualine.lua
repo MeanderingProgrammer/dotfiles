fe9fbfab95a5489a51cc67855660c855f035d101
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-04-14 15:09:18 -0700

    Include bufnr along with LSP info in lualine

--- .config/nvim/lua/mp/plugins/lualine.lua
+++ .config/nvim/lua/mp/plugins/lualine.lua
@@ -2,18 +2,23 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
-        local function lsp_clients()
-            local buffer_clients = vim.lsp.get_active_clients({ bufnr = 0 })
+        local function buf_lsp_info()
+            local bufnr = vim.api.nvim_get_current_buf()
+            local buffer_part = string.format('(%d)', bufnr)
+
+            local buffer_clients = vim.lsp.get_active_clients({ bufnr = bufnr })
             local clients = vim.tbl_map(function(client)
                 return client.name
             end, buffer_clients)
-            return vim.fn.join(clients, ' ')
+            local clients_part = table.concat(clients, ' ')
+
+            return table.concat({ buffer_part, clients_part }, ' ')
         end
         local filename_section = { 'filename', path = 1 }
         require('lualine').setup({
             sections = {
                 lualine_c = { filename_section },
-                lualine_x = { lsp_clients, 'filetype' },
+                lualine_x = { buf_lsp_info, 'filetype' },
             },
             inactive_sections = {
                 lualine_c = { filename_section },
