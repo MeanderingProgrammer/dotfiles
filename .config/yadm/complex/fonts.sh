c254af5bf3b441fb1e25212bddec525499b415ec
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-05-28 20:08:19 -0700

    Update font setup logic

--- .config/yadm/complex/fonts.sh
+++ .config/yadm/complex/fonts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# https://github.com/githubnext/monaspace/blob/main/util/install_linux.sh
 font_dir="$HOME/.local/share/fonts"
-mkdir -p $font_dir
-if [[ ! -f "${font_dir}/MonaspaceNeon-Regular.otf" ]]; then
-    wget https://github.com/githubnext/monaspace/releases/download/v1.000/monaspace-v1.000.zip
-    unzip -d temp-fonts monaspace-v1.000.zip
-    cp temp-fonts/monaspace-v1.000/fonts/otf/* $font_dir
-    rm -rf monaspace-v1.000.zip temp-fonts
-    fc-cache -f
-fi
+[[ ! -d $font_dir ]] && mkdir -p $font_dir
+
+font_names=("MonaspaceNeon-Regular.otf" "segoeui.ttf")
+for font_name in "${font_names[@]}"; do
+    if [[ ! -f "${font_dir}/${font_name}" ]]; then
+        cp "$(dirname $0)/complex/${font_name}" "${font_dir}"
+        fc-cache -f
+    fi
+done
