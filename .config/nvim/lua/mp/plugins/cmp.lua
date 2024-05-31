6f0058db0342d7bd0ceaa221ad3d945f0e497202
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-05-30 22:43:16 -0700

    Use native snippets

--- .config/nvim/lua/mp/plugins/cmp.lua
+++ .config/nvim/lua/mp/plugins/cmp.lua
@@ -1,106 +1,87 @@
 return {
-    {
-        'L3MON4D3/LuaSnip',
-        version = 'v2.*',
-        build = 'make install_jsregexp',
-        dependencies = { 'rafamadriz/friendly-snippets' },
-        config = function()
-            local luasnip = require('luasnip')
-            require('luasnip.loaders.from_vscode').lazy_load()
-            luasnip.config.setup({})
-        end,
+    'hrsh7th/nvim-cmp',
+    event = 'InsertEnter',
+    dependencies = {
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-cmdline',
+        'hrsh7th/cmp-path',
     },
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            'saadparwaiz1/cmp_luasnip',
-            'hrsh7th/cmp-buffer',
-            'hrsh7th/cmp-cmdline',
-            'hrsh7th/cmp-path',
-        },
-        opts = {
-            sources = {},
-        },
-        config = function(_, opts)
-            local luasnip = require('luasnip')
-
-            local cmp = require('cmp')
-            local compare = require('cmp.config.compare')
-
-            vim.list_extend(opts.sources, {
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' },
-                { name = 'buffer' },
-                { name = 'path' },
-            })
-
-            cmp.setup({
-                sources = cmp.config.sources(opts.sources),
-                window = {
-                    completion = cmp.config.window.bordered({ border = 'rounded' }),
-                    documentation = cmp.config.window.bordered({ border = 'rounded' }),
-                },
-                mapping = cmp.mapping.preset.insert({
-                    ['<cr>'] = cmp.mapping.confirm({ select = true }),
-                    ['<C-space>'] = cmp.mapping.complete(),
-                    ['<C-n>'] = cmp.mapping.select_next_item(),
-                    ['<C-p>'] = cmp.mapping.select_prev_item(),
-                    ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-                    ['<tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-                        elseif luasnip.expand_or_jumpable() then
-                            luasnip.expand_or_jump()
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                    ['<S-tab>'] = cmp.mapping(function(fallback)
-                        if cmp.visible() then
-                            cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
-                        elseif luasnip.jumpable(-1) then
-                            luasnip.jump(-1)
-                        else
-                            fallback()
-                        end
-                    end, { 'i', 's' }),
-                }),
-                sorting = {
-                    priority_weight = 2,
-                    comparators = {
-                        -- Default: https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/config/default.lua
-                        -- With sort_text added
-                        compare.exact,
-                        compare.score,
-                        compare.sort_text,
-                        compare.offset,
-                        compare.recently_used,
-                        compare.locality,
-                        compare.kind,
-                        compare.length,
-                        compare.order,
-                    },
-                },
-                snippet = {
-                    expand = function(args)
-                        luasnip.lsp_expand(args.body)
-                    end,
-                },
-                experimental = {
-                    ghost_text = true,
-                },
-            })
-
-            cmp.setup.cmdline({ '/', '?' }, {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = { { name = 'buffer' } },
-            })
-
-            cmp.setup.cmdline(':', {
-                mapping = cmp.mapping.preset.cmdline(),
-                sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } }),
-            })
-        end,
+    opts = {
+        sources = {},
     },
+    config = function(_, opts)
+        local cmp = require('cmp')
+        local compare = require('cmp.config.compare')
+
+        vim.list_extend(opts.sources, {
+            { name = 'nvim_lsp' },
+            { name = 'buffer' },
+            { name = 'path' },
+        })
+
+        cmp.setup({
+            sources = cmp.config.sources(opts.sources),
+            window = {
+                completion = cmp.config.window.bordered({ border = 'rounded' }),
+                documentation = cmp.config.window.bordered({ border = 'rounded' }),
+            },
+            mapping = cmp.mapping.preset.insert({
+                ['<cr>'] = cmp.mapping.confirm({ select = true }),
+                ['<C-space>'] = cmp.mapping.complete(),
+                ['<C-n>'] = cmp.mapping.select_next_item(),
+                ['<C-p>'] = cmp.mapping.select_prev_item(),
+                ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                ['<tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
+                    elseif vim.snippet.jumpable(1) then
+                        vim.snippet.jump(1)
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+                ['<S-tab>'] = cmp.mapping(function(fallback)
+                    if cmp.visible() then
+                        cmp.select_prev_item({ behavior = cmp.SelectBehavior.Select })
+                    elseif vim.snippet.jumpable(-1) then
+                        vim.snippet.jump(-1)
+                    else
+                        fallback()
+                    end
+                end, { 'i', 's' }),
+            }),
+            sorting = {
+                priority_weight = 2,
+                comparators = {
+                    -- Default: https://github.com/hrsh7th/nvim-cmp/blob/main/lua/cmp/config/default.lua
+                    -- With sort_text added
+                    compare.exact,
+                    compare.score,
+                    compare.sort_text,
+                    compare.offset,
+                    compare.recently_used,
+                    compare.locality,
+                    compare.kind,
+                    compare.length,
+                    compare.order,
+                },
+            },
+            snippet = {
+                expand = function(args)
+                    vim.snippet.expand(args.body)
+                end,
+            },
+        })
+
+        cmp.setup.cmdline({ '/', '?' }, {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = { { name = 'buffer' } },
+        })
+
+        cmp.setup.cmdline(':', {
+            mapping = cmp.mapping.preset.cmdline(),
+            sources = cmp.config.sources({ { name = 'path' } }, { { name = 'cmdline' } }),
+        })
+    end,
 }
--- .config/nvim/lua/mp/plugins/lsp.lua
+++ .config/nvim/lua/mp/plugins/lsp.lua
@@ -83,8 +83,12 @@ return {
             end,
         })
 
-        local capabilities = vim.lsp.protocol.make_client_capabilities()
-        capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+        local capabilities = vim.tbl_deep_extend(
+            'force',
+            {},
+            vim.lsp.protocol.make_client_capabilities(),
+            require('cmp_nvim_lsp').default_capabilities()
+        )
 
         require('mason').setup({})
         require('mason-lspconfig').setup({
