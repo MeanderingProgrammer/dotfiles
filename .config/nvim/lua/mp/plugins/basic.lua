59bf83cc5d72e26344b3cd1040163806e737d84a
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-05-30 23:02:50 -0700

    Remove zen-mode plugin

--- .config/nvim/lua/mp/plugins/basic.lua
+++ .config/nvim/lua/mp/plugins/basic.lua
@@ -2,6 +2,5 @@ return {
     { 'mg979/vim-visual-multi' },
     { 'tpope/vim-fugitive' },
     { 'tpope/vim-sleuth' },
-    { 'folke/zen-mode.nvim', config = true },
     { 'j-hui/fidget.nvim', config = true },
 }
