3a17388aa28ac17bc26585385b946af515902102
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-05-31 09:04:18 -0700

    Add json5 treesitter parser

--- .config/nvim/lua/mp/plugins/lang/json.lua
+++ .config/nvim/lua/mp/plugins/lang/json.lua
@@ -2,7 +2,7 @@ return {
     {
         'nvim-treesitter/nvim-treesitter',
         opts = function(_, opts)
-            vim.list_extend(opts.ensure_installed, { 'jq', 'json', 'jsonc' })
+            vim.list_extend(opts.ensure_installed, { 'jq', 'json', 'json5', 'jsonc' })
         end,
     },
     {
