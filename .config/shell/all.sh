57ac88927f3628eab2522fc4974aece2212c36bf
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-06-03 18:30:21 -0700

    Update remote sync script to check for forks and validate repo owner. Source custom.sh.

--- .config/shell/all.sh
+++ .config/shell/all.sh
@@ -1,6 +1,12 @@
-# ---- Main setup ---- #
-source ~/.config/shell/aliases.sh
-source ~/.config/shell/software.sh
+# ---- Common setup ---- #
+shell_home="${XDG_CONFIG_HOME}/shell"
+
+source "${shell_home}/aliases.sh"
+source "${shell_home}/software.sh"
+
+# ---- Computer specific setup ---- #
+custom_shell="${shell_home}/custom.sh"
+[[ -f $custom_shell ]] && source "${custom_shell}"
 
 # ---- Only run if not already in TMUX ---- #
-[[ -z $TMUX ]] && source ~/.config/shell/tmux.sh
+[[ -z $TMUX ]] && source "${shell_home}/tmux.sh"
