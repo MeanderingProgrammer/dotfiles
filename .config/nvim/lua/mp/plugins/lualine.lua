6c57d73a8d3bafb640ec9e502fe7fd118aea5f76
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-06-19 00:30:31 -0700

    Update lualine config

--- .config/nvim/lua/mp/plugins/lualine.lua
+++ .config/nvim/lua/mp/plugins/lualine.lua
@@ -3,21 +3,25 @@ return {
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
         local function buf_lsp_info()
-            local bufnr = vim.api.nvim_get_current_buf()
-            return vim.iter(vim.lsp.get_clients({ bufnr = bufnr }))
-                :map(function(client)
-                    return client.name
-                end)
-                :join(' ')
+            if vim.api.nvim_win_get_width(0) > 100 then
+                local bufnr = vim.api.nvim_get_current_buf()
+                return vim.iter(vim.lsp.get_clients({ bufnr = bufnr }))
+                    :map(function(client)
+                        return client.name
+                    end)
+                    :join(' ')
+            else
+                return ''
+            end
         end
-        local filename_section = { 'filename', path = 1 }
+        local filename = { 'filename', path = 1, shorting_target = 100 }
         require('lualine').setup({
             sections = {
-                lualine_c = { filename_section },
+                lualine_c = { filename },
                 lualine_x = { '%n', buf_lsp_info, 'filetype' },
             },
             inactive_sections = {
-                lualine_c = { filename_section },
+                lualine_c = { filename },
             },
             extensions = { 'oil' },
         })
