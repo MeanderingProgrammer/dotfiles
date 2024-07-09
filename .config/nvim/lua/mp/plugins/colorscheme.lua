98a02fe32823826e6aed49c5176f974413df2c59
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-07-09 13:35:27 -0700

    Use tmux-256 instead of screen-256 to fix italics

--- .config/nvim/lua/mp/plugins/colorscheme.lua
+++ .config/nvim/lua/mp/plugins/colorscheme.lua
@@ -2,9 +2,7 @@ return {
     'rose-pine/neovim',
     name = 'rose-pine',
     config = function()
-        require('rose-pine').setup({
-            styles = { italic = false },
-        })
+        require('rose-pine').setup({})
         vim.cmd.colorscheme('rose-pine')
     end,
 }
