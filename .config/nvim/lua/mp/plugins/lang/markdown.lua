e0974d488fe2cc456b2e5fe245894dca46a963b8
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2024-07-12 06:48:45 -0700

    Remove nofile exclude for markdown render

--- .config/nvim/lua/mp/plugins/lang/markdown.lua
+++ .config/nvim/lua/mp/plugins/lang/markdown.lua
@@ -30,9 +30,7 @@ return {
         dev = true,
         dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' },
         config = function()
-            require('render-markdown').setup({
-                exclude = { buftypes = { 'nofile' } },
-            })
+            require('render-markdown').setup({})
         end,
     },
 }
