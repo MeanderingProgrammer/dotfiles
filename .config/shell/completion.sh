c37411459f19079d8cabd4bee0bb1c0e2ed0fd28
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-03-13 12:17:58 -0700

    check if click exists via pip before generating completions

--- .config/shell/completion.sh
+++ .config/shell/completion.sh
@@ -14,12 +14,24 @@ autoload -Uz compinit && compinit -d "${zsh_cache_home}/compdump"
 zsh_comp_home="${XDG_DATA_HOME}/completions"
 [[ ! -d $zsh_comp_home ]] && mkdir -p $zsh_comp_home
 
+check_python_env() {
+    # Check if the result is already cached
+    if [[ -n $PYTHON_ENV ]]; then
+        return $PYTHON_ENV
+    fi
+    PYTHON_ENV=0
+    [[ -x "$(command -v python)" ]] || PYTHON_ENV=1
+    [[ -x "$(command -v pip)" ]] || PYTHON_ENV=1
+    [[ -n "$(pip show click 2> /dev/null)" ]] || PYTHON_ENV=1
+    return $PYTHON_ENV
+}
+
 click_completion() {
-    [[ -x "$(command -v python)" ]] || return
     local completion_file="${zsh_comp_home}/${1}-complete.zsh"
     # Generate completions if they don't exist or we're outside of tmux
     if [[ ! -f $completion_file || -z $TMUX ]]; then
-        click_variable=$(echo ${1} | tr '[:lower:]' '[:upper:]' | tr '-' '_')
+        check_python_env || return
+        local click_variable=$(echo ${1} | tr '[:lower:]' '[:upper:]' | tr '-' '_')
         eval "_${click_variable}_COMPLETE=zsh_source ${1} > ${completion_file}"
     fi
     source "${completion_file}"
