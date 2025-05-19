cf6a2682ef96e6b42802dbd6855092c1e13d087f
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-05-19 14:27:06 -0700

    disable async treesitter parsing for time being

--- .config/nvim/lua/mp/config/options.lua
+++ .config/nvim/lua/mp/config/options.lua
@@ -1,82 +1,86 @@
--- Remap space as leader key
+-- remap space as leader key
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- OS information
+-- TODO: remove once flickering issue is resolved
+-- https://github.com/neovim/neovim/issues/32660
+vim.g._ts_force_sync_parsing = true
+
+-- os information
 vim.g.mac = vim.fn.has('mac') == 1
 vim.g.linux = vim.fn.has('linux') == 1 or vim.fn.has('wsl') == 1
 vim.g.android = vim.fn.has('android') == 1
 
--- Disable netrw
+-- use an opaque or transparent colorscheme
+vim.g.opaque = true
+
+-- completion plugin, true = nvim-cmp, false = blink.cmp
+vim.g.cmp = true
+
+-- disable python ftplugin keymaps
+vim.g.no_python_maps = true
+
+-- disable netrw
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Use an opaque or transparent colorscheme
-vim.g.opaque = true
-
--- Completion plugin, true = nvim-cmp, false = blink.cmp
-vim.g.cmp = true
-
--- Disable python ftplugin keymaps
-vim.g.no_python_maps = true
-
--- Color settings
+-- color
 vim.opt.termguicolors = true
 vim.opt.cursorline = true
 
--- Line number settings
+-- line number
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.statuscolumn = '%s%=%{v:relnum?v:relnum:v:lnum} '
 
--- Mode is already in status line plugin
+-- mode is already in status line plugin
 vim.opt.showmode = false
 
--- Search settings
+-- search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- Set wait time to complete mapping
+-- wait time to complete mapping
 vim.opt.timeoutlen = 500
 
--- Set wait time for detecting changes
+-- wait time for detecting changes
 vim.opt.updatetime = 250
 
--- Save / Backup / Undo settings
+-- save / backup / undo
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undofile = true
 
--- Split to right / below by default
+-- split to right / below by default
 vim.opt.splitright = true
 vim.opt.splitbelow = true
 
--- Better completion experience
+-- better completion experience
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
--- Indentation settings
+-- indentation
 vim.opt.smartindent = true
 
--- Tab / Space settings
+-- tab / space
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
--- Keep some context in view
+-- keep context in view
 vim.opt.scrolloff = 8
 vim.opt.sidescrolloff = 8
 
--- Use system clipboard
+-- use system clipboard
 vim.opt.clipboard = 'unnamedplus'
 
--- Spell checking
+-- spell checking
 vim.opt.spell = true
 
--- Border style of floating windows
+-- border style of floating windows
 vim.opt.winborder = 'rounded'
 
--- Disable default providers
+-- disable default providers
 for _, provider in ipairs({ 'python3', 'ruby', 'node', 'perl' }) do
     vim.g['loaded_' .. provider .. '_provider'] = 0
 end
