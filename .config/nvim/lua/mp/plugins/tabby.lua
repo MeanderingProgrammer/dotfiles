16f4b50ebc2e93989ab4da4b69580887ebbb70b1
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-05-29 08:00:07 -0700

    use mini.icons for tabby

--- .config/nvim/lua/mp/plugins/tabby.lua
+++ .config/nvim/lua/mp/plugins/tabby.lua
@@ -1,6 +1,6 @@
 return {
     'nanozuki/tabby.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    dependencies = { 'echasnovski/mini.nvim' },
     config = function()
         require('tabby.tabline').use_preset('active_wins_at_tail', {})
     end,
