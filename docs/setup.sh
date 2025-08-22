e94d96a8e7eba38bc3a59e12aa60d6a6e7259e48
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-08-22 09:56:38 -0700

    rely more on system state when setting up rather than uname values

--- docs/setup.sh
+++ docs/setup.sh
@@ -7,17 +7,24 @@ SUCCESS=32
 SKIP=33
 TITLE=35
 INFO=36
+
 notify() {
     echo -e "\033[0;${1}m${2}\033[0m"
 }
 
-system_type=$(uname -s)
+has_command() {
+    [[ -x "$(command -v ${1})" ]] || {
+        notify $SKIP "  missing: ${1}"
+        return 1
+    }
+}
+
 system_os=$(uname -o)
 
 main() {
-    notify $TITLE "System: ${system_type}-${system_os}"
+    notify $TITLE "system: ${system_os}"
     if [[ "${#}" -ne 1 ]]; then
-        notify $FAIL "Usage: <command>"
+        notify $FAIL "usage: <command>"
         exit 1
     fi
     case ${1} in
@@ -29,18 +36,16 @@ main() {
         "yadm") do_yadm ;;
         "clean") do_clean ;;
         *)
-            notify $FAIL "Unknown command: ${1}"
-            notify $FAIL "Commands: deps, shell, limit, brew, git, yadm, clean"
+            notify $FAIL "unknown command: ${1}"
+            notify $FAIL "valid commands: deps, shell, limit, brew, git, yadm, clean"
             exit 1
             ;;
     esac
 }
 
 do_deps() {
-    notify $TITLE "Installing dependencies"
-    if [[ "${system_type}" == "Darwin" ]]; then
-        notify $SKIP "  Skipping: none"
-    elif [[ "${system_os}" == "Android" ]]; then
+    notify $TITLE "start: installing dependencies"
+    if has_command "pkg"; then
         pkg install --yes \
           bat \
           clang \
@@ -69,8 +74,8 @@ do_deps() {
           xz-utils \
           yadm \
           zsh
-        notify $SUCCESS "  Success"
-    elif [[ "${system_type}" == "Linux" ]]; then
+        notify $SUCCESS "  success"
+    elif has_command "apt"; then
         sudo apt --yes install \
           bubblewrap \
           build-essential \
@@ -88,141 +93,136 @@ do_deps() {
           libxmlsec1-dev \
           llvm \
           make \
-          texlive \
-          texlive-xetex \
           tk-dev \
           wget \
           wl-clipboard \
+          xclip \
           xz-utils \
           zlib1g-dev \
           zsh
-        notify $SUCCESS "  Success"
+        notify $SUCCESS "  success"
     else
-        notify $FAIL "  Error: unhandled system type"
-        exit 1
+        notify $SKIP "  skip"
     fi
 }
 
 do_shell() {
-    notify $TITLE "Changing shell to zsh"
-    shell_type=$(basename "$SHELL")
-    if [[ "${shell_type}" == "zsh" ]]; then
-        notify $SKIP "  Skipping: already done"
-    elif [[ "${shell_type}" == "bash" ]]; then
-        if [[ "${system_type}" == "Linux" ]]; then
-            chsh -s $(which zsh)
-            notify $SUCCESS "  Success: restart terminal"
-        else
-            notify $FAIL "  Error: unhandled system type"
-            exit 1
-        fi
+    notify $TITLE "start: changing shell to zsh"
+    local shell_type=$(basename "$SHELL")
+    if [[ "${shell_type}" == "bash" ]]; then
+        chsh -s $(which zsh)
+        notify $SUCCESS "  success: restart system"
+    elif [[ "${shell_type}" == "zsh" ]]; then
+        notify $SKIP "  skip"
     else
-        notify $FAIL "  Error: unhandled shell type ${shell_type}"
+        notify $FAIL "  error: unhandled shell type ${shell_type}"
         exit 1
     fi
 }
 
 do_limit() {
-    notify $TITLE "Increasing limits"
-    if [[ "${system_type}" == "Darwin" ]]; then
-        local limit_directory="/Library/LaunchDaemons"
+    notify $TITLE "start: increasing limits"
+    local limit_directory="/Library/LaunchDaemons"
+    if [[ -d ${limit_directory} ]]; then
         local limit_file="limit.maxfiles.plist"
         sudo cp "$HOME/docs/${limit_file}" "${limit_directory}"
         sudo chown root:wheel "${limit_directory}/${limit_file}"
         sudo launchctl load -w "${limit_directory}/${limit_file}"
-        notify $SUCCESS "  Success"
+        notify $SUCCESS "  success"
     else
-        notify $SKIP "  Skipping: not Darwin"
+        notify $SKIP "  skip: not Darwin"
     fi
 }
 
 do_homebrew() {
-    notify $TITLE "Installing homebrew"
-    if [[ -x "$(command -v brew)" ]]; then
-        notify $SKIP "  Skipping: already done"
+    notify $TITLE "start: installing homebrew"
+    if has_command "brew"; then
+        notify $SKIP "  skip: already done"
     elif [[ "${system_os}" == "Android" ]]; then
-        notify $SKIP "  Skipping: android"
+        notify $SKIP "  skip: android"
     else
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-        notify $SUCCESS "  Success"
+        notify $SUCCESS "  success"
     fi
     evaluate_homebrew
 }
 
 evaluate_homebrew() {
-    notify $TITLE "Evaluating homebrew"
-    if [[ -x "$(command -v brew)" ]]; then
-        notify $SKIP "  Skipping: already done"
+    notify $TITLE "start: evaluating homebrew"
+    if has_command "brew"; then
+        notify $SKIP "  skip: already done"
     elif [[ "${system_os}" == "Android" ]]; then
-        notify $SKIP "  Skipping: android"
+        notify $SKIP "  skip: android"
     else
-        if [[ "${system_type}" == "Darwin" ]]; then
-            eval "$(/opt/homebrew/bin/brew shellenv)"
-        elif [[ "${system_type}" == "Linux" ]]; then
-            eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+        local mac_init="/opt/homebrew/bin/brew"
+        local linux_init="/home/linuxbrew/.linuxbrew/bin/brew"
+        if [[ -x ${mac_init} ]]; then
+            eval "$($mac_init shellenv)"
+            notify $SUCCESS "  success"
+        elif [[ -x ${linux_init} ]]; then
+            eval "$($linux_init shellenv)"
+            notify $SUCCESS "  success"
         else
-            notify $FAIL "  Error: unhandled system type"
+            notify $FAIL "  error: missing init"
             exit 1
         fi
-        notify $SUCCESS "  Success"
     fi
 }
 
 brew_install() {
+    notify $TITLE "start: installing ${1} with homebrew"
     evaluate_homebrew
-    notify $TITLE "Installing ${1} with homebrew"
     if [[ "${system_os}" == "Android" ]]; then
-        notify $SKIP "  Skipping: android"
+        notify $SKIP "  skip: android"
     else
         brew install ${1}
-        notify $SUCCESS "  Success"
+        notify $SUCCESS "  success"
     fi
 }
 
 setup_file() {
-    notify $TITLE "Creating empty file: ${1}"
+    notify $TITLE "start: creating empty file ${1}"
     if [[ -f ${1} ]]; then
-        notify $SKIP "  Skipping: already done"
+        notify $SKIP "  skip: already done"
     else
         local directory=$(dirname ${1})
         mkdir -p ${directory}
         touch ${1}
-        notify $SUCCESS "  Success"
+        notify $SUCCESS "  success"
     fi
 }
 
 setup_ssh() {
     # https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
-    notify $TITLE "Adding hosts: ${2}"
+    notify $TITLE "start: adding hosts ${2}"
     local hosts=$(cat ${1} | grep ${2})
     if [[ -z "${hosts}" ]]; then
         ssh-keyscan ${2} >> ${1}
-        notify $SUCCESS "  Success"
+        notify $SUCCESS "  success"
     else
-        notify $SKIP "  Skipping: already done"
+        notify $SKIP "  skip: already done"
     fi
 
     # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
-    notify $TITLE "Generating SSH key: ${2}"
+    notify $TITLE "start: generating SSH key ${2}"
     local ssh_file="$HOME/.ssh/${3}"
     if [[ -f ${ssh_file} ]]; then
-        notify $SKIP "  Skipping: already done"
+        notify $SKIP "  skip: already done"
     else
         ssh-keygen -f ${ssh_file} -t ed25519 -C "meanderingprogrammer@gmail.com"
         eval "$(ssh-agent -s)"
-        notify $SUCCESS "  Success"
+        notify $SUCCESS "  success"
     fi
 
     # https://docs.github.com/en/authentication/connecting-to-github-with-ssh/adding-a-new-ssh-key-to-your-github-account
-    notify $TITLE "Copy command: ${2}"
+    notify $TITLE "start: copy command ${2}"
     local copy_command
-    if [[ "${system_type}" == "Darwin" ]]; then
+    if has_command "pbcopy"; then
         copy_command="pbcopy"
-    elif [[ "${system_type}" == "Linux" ]]; then
-        copy_command="wl-copy"
+    elif [[ "${XDG_SESSION_TYPE}" == "x11" ]]; then
+        copy_command="xclip -selection clipboard"
     else
-        notify $FAIL "  Error: unhandled system type"
-        exit 1
+        copy_command="wl-copy"
     fi
     notify $INFO "  cat ${ssh_file}.pub | ${copy_command}"
 }
@@ -244,20 +244,20 @@ do_yadm() {
     brew_install "yadm"
 
     # https://yadm.io/docs/bootstrap
-    notify $TITLE "Cloning dotfiles repo"
+    notify $TITLE "start: cloning dotfiles repo"
     local yadm_directory="$HOME/.config/yadm"
     if [[ -d ${yadm_directory} ]]; then
-        notify $SKIP "  Skipping: already done"
+        notify $SKIP "  skip: already done"
     else
         yadm clone --bootstrap git@github.com:MeanderingProgrammer/dotfiles.git
-        notify $SUCCESS "  Success"
+        notify $SUCCESS "  success"
     fi
 }
 
 do_clean() {
-    notify $TITLE "Deleting setup.sh"
+    notify $TITLE "start: deleting setup.sh"
     rm -rf "setup.sh"
-    notify $SUCCESS "  Success"
+    notify $SUCCESS "  success"
 }
 
 main "$@"
