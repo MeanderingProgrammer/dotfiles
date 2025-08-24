b0305c92fa4c5405c2de7212e4f185eb578f26fe
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-08-24 15:45:18 -0700

    minor edits / remove tabby

--- .config/nvim/lua/mp/options.lua
+++ .config/nvim/lua/mp/options.lua
@@ -4,6 +4,7 @@ vim.opt.termguicolors = true
 -- line number
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.signcolumn = 'yes'
 vim.opt.statuscolumn = '%s%=%{v:relnum?v:relnum:v:lnum} '
 
 -- mode is in status line
@@ -19,22 +20,22 @@ vim.opt.timeoutlen = 500
 -- wait time for detecting changes
 vim.opt.updatetime = 250
 
--- save / backup / undo
-vim.opt.swapfile = false
+-- backup / swap / undo
 vim.opt.backup = false
+vim.opt.swapfile = false
 vim.opt.undofile = true
 
--- split right / below by default
-vim.opt.splitright = true
+-- split below / right by default
 vim.opt.splitbelow = true
+vim.opt.splitright = true
 
 -- better completion experience
 vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 
 -- indentation
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
--- .config/nvim/lua/mp/plugins/tabby.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'nanozuki/tabby.nvim',
-    dependencies = { 'echasnovski/mini.nvim' },
-    config = function()
-        require('tabby.tabline').use_preset('active_wins_at_tail', {})
-    end,
-}
