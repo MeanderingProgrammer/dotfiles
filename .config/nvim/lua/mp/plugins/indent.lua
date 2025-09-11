88acbabc1798acf409c0b459ee48043bee3da24c
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2025-09-10 17:09:30 -0700

    disable indent guides in markdown

--- .config/nvim/lua/mp/plugins/indent.lua
+++ .config/nvim/lua/mp/plugins/indent.lua
@@ -2,7 +2,9 @@ return {
     'lukas-reineke/indent-blankline.nvim',
     config = function()
         require('ibl').setup({
-            exclude = { filetypes = { 'dashboard' } },
+            exclude = {
+                filetypes = { 'dashboard', 'markdown' },
+            },
             scope = { enabled = false },
         })
     end,
