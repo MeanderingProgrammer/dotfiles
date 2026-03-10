5c660c056de14ccaeebd2f7b8b3ca95831d9d595
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2026-03-10 11:16:12 -0700

    update dashboard root directory

--- .config/nvim/lua/mp/plugins/dashboard.lua
+++ .config/nvim/lua/mp/plugins/dashboard.lua
@@ -2,8 +2,8 @@ local utils = require('mp.lib.utils')
 
 ---@return string[]
 local function git_directories()
-    ---@type string[]
-    local cmd = { 'find', vim.fs.normalize('~/dev/repos/personal') }
+    local root = vim.g.personal and '~/dev/repos/personal' or '~/dev/repos/work'
+    local cmd = { 'find', vim.fs.normalize(root) } ---@type string[]
     vim.list_extend(cmd, { '-type', 'd', '-name', '.git', '-maxdepth', '2' })
     local result = {} ---@type string[]
     local paths = utils.split(utils.system(cmd), '\n')
