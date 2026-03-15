020acd3040b0df6014e1835ed3ca1e5edce9b108
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2026-03-14 19:53:58 -0700

    revert back to basedpyright

--- .config/nvim/lua/mp/lang/python.lua
+++ .config/nvim/lua/mp/lang/python.lua
@@ -6,14 +6,14 @@ require('mp.lib.lang').add({
         requirements = { install = true },
     },
     tool = {
-        ['ty'] = { install = vim.g.pc and vim.g.has.pip },
+        ['basedpyright'] = { install = vim.g.pc and vim.g.has.pip },
         ['ruff'] = { install = vim.g.pc and vim.g.has.pip },
         ['pyright'] = { install = not vim.g.pc and vim.g.has.npm },
         ['black'] = { install = not vim.g.pc and vim.g.has.pip },
         ['isort'] = { install = not vim.g.pc and vim.g.has.pip },
     },
     lsp = {
-        ty = {},
+        basedpyright = {},
         pyright = {},
     },
     format = {
