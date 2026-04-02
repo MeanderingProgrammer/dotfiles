825ab0ae49c001d3691017693f3e7033ec766a43
MeanderingProgrammer <meanderingprogrammer@gmail.com>
2026-04-02 02:09:10 -0700

    remove branch from nvim-treesitter-textobjects

--- .config/nvim/lua/mp/plugins/nvim-treesitter-textobjects.lua
+++ .config/nvim/lua/mp/plugins/nvim-treesitter-textobjects.lua
@@ -1,6 +1,5 @@
 return {
     'nvim-treesitter/nvim-treesitter-textobjects',
-    branch = 'main',
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
         require('mp.configs.nvim-treesitter-textobjects')
